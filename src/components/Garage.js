f7047d0a975ebaaea231016c10a4299fa351ada6
Juan Carranza <juan.carranza1804@gmail.com>
2022-07-24 08:02:28 -0600

    Renderizacion condicional

--- src/components/Garage.js
+++ src/components/Garage.js
@@ -1,7 +1,7 @@
 import Carro from "./Carro";
 
 let carros= [
-    {
+     {
         modelo: "Hilux",
         color: "gris"
     },
@@ -12,16 +12,27 @@ let carros= [
     {
         modelo: "Corolla",
         color: "rojo"
-    }
+    } 
 ];
 const Garage = () =>{
     return (<>
         <h1>Estos son los carros de mi garage: </h1>
-        {carros.map((carro, index) => {
-            return <Carro key={`${index}-carro-${carro.modelo}`} modelo={carro.modelo} color={carro.color} />
-        })}
-{/*         <Carro modelo={"Hilux"} color={"gris"} />
-        <Carro modelo={"Yaris"} color={"negro"} /> */}
+        {  
+        }
+
+        {//renderizacion condicional usando &&
+            carros.length === 0 && <p>No hay carros en el garage</p>
+        }
+        {//renderizacion condicional usando operador ternario ?
+            carros.length === 0 ? (<p>No hay carros en el garage</p>) : 
+            (carros.map((carro, index) => {
+                    return <Carro key={`${index}-carro-${carro.modelo}`} modelo={carro.modelo} color={carro.color} />//renderizacion dinamica 
+                })
+            )
+        }
+        {/*         <Carro modelo={"Hilux"} color={"gris"} />
+            <Carro modelo={"Yaris"} color={"negro"} /> */
+        }
     </>
     );
 };
